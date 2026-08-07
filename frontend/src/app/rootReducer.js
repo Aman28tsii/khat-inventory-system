3753010cc943d13f26e034f610e0e1949341112c
Aman28tsii <aman28tsii@gmail.com>
2026-08-07 11:01:27 -0700

    test: Add second half (Purchases, Sales, Transfers, Dashboard)

--- frontend/src/app/rootReducer.js
+++ frontend/src/app/rootReducer.js
@@ -1,18 +1,16 @@
 ﻿import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/slices/authSlice';
-import usersReducer from '../features/users/slices/usersSlice';
-import rolesReducer from '../features/roles/slices/rolesSlice';
-import branchesReducer from '../features/branches/slices/branchesSlice';
-import productReducer from '../features/inventory/slices/productSlice';
-import customersReducer from '../features/customers/slices/customerSlice';
+import purchasesReducer from '../features/purchases/slices/purchaseSlice';
+import salesReducer from '../features/sales/slices/saleSlice';
+import transfersReducer from '../features/transfers/slices/transferSlice';
+import dashboardReducer from '../features/dashboard/slices/dashboardSlice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  users: usersReducer,
-  roles: rolesReducer,
-  branches: branchesReducer,
-  products: productReducer,
-  customers: customersReducer,
+  purchases: purchasesReducer,
+  sales: salesReducer,
+  transfers: transfersReducer,
+  dashboard: dashboardReducer,
 });
 
 export default rootReducer;
