a8b536d0b9aa2f55380f8e92ef0944925857cb74
Aman28tsii <aman28tsii@gmail.com>
2026-08-07 11:03:38 -0700

    test: Only auth + users

--- frontend/src/app/rootReducer.js
+++ frontend/src/app/rootReducer.js
@@ -1,16 +1,10 @@
 ﻿import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/slices/authSlice';
-import purchasesReducer from '../features/purchases/slices/purchaseSlice';
-import salesReducer from '../features/sales/slices/saleSlice';
-import transfersReducer from '../features/transfers/slices/transferSlice';
-import dashboardReducer from '../features/dashboard/slices/dashboardSlice';
+import usersReducer from '../features/users/slices/usersSlice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  purchases: purchasesReducer,
-  sales: salesReducer,
-  transfers: transfersReducer,
-  dashboard: dashboardReducer,
+  users: usersReducer,
 });
 
 export default rootReducer;
