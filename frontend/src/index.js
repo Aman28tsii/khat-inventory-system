237fae8eaea4c00c3d1f8067f962eb6d48a6e168
Aman28tsii <aman28tsii@gmail.com>
2026-08-07 03:21:22 -0700

    Fix index.js without reportWebVitals call

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,16 +8,13 @@ import { Toaster } from 'react-hot-toast';
 import App from './App';
 import store from './app/store';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
-// React Query Client
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
       retry: 1,
-      staleTime: 5 * 60 * 1000, // 5 minutes
-      cacheTime: 10 * 60 * 1000, // 10 minutes
+      staleTime: 5 * 60 * 1000,
     },
   },
 });
@@ -61,8 +58,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
