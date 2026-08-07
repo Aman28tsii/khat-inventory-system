cf00fa45c7932e4586ef2d2839bc347a9e447f33
Aman28tsii <aman28tsii@gmail.com>
2026-08-07 10:55:20 -0700

    test: Add first half (Users, Roles, Branches, Products, Customers)

--- frontend/src/app/rootReducer.js
+++ frontend/src/app/rootReducer.js
@@ -1,8 +1,18 @@
 ﻿import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/slices/authSlice';
+import usersReducer from '../features/users/slices/usersSlice';
+import rolesReducer from '../features/roles/slices/rolesSlice';
+import branchesReducer from '../features/branches/slices/branchesSlice';
+import productReducer from '../features/inventory/slices/productSlice';
+import customersReducer from '../features/customers/slices/customerSlice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  users: usersReducer,
+  roles: rolesReducer,
+  branches: branchesReducer,
+  products: productReducer,
+  customers: customersReducer,
 });
 
 export default rootReducer;
