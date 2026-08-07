f7124c4038ff0fbf57abfd3c3a35bd494f7c6e23
Aman28tsii <aman28tsii@gmail.com>
2026-08-07 08:41:59 -0700

    Fix rootReducer: remove circular dependencies

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,12 +3,6 @@ import rootReducer from './rootReducer';
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
-      },
-    }),
   devTools: process.env.NODE_ENV !== 'production',
 });
 
